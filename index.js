824fab081aea5d7c8b1c7b6d46cf7906ab4aeaad
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-12-19 14:49:35 +0000

    updated questions

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ What should we return when needle is an empty string? This is a great question t
 
 For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 
+Example 1)
+Input: haystack = "hello", needle = "ll"
+Output: 2
